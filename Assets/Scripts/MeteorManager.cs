340bdd78abe16d7283a976478e7d0d89469e9256
Justin Basinger <justin.m.basinger@gmail.com>
2015-04-19 17:13:21 -0400

    Fixing something dumb and upping % of power chance

--- Assets/Scripts/MeteorManager.cs
+++ Assets/Scripts/MeteorManager.cs
@@ -40,7 +40,7 @@ public class MeteorManager : MonoBehaviour {
 			
 			rndMeteorStart = new Vector3(Mathf.Cos(rndAngle)*(meteorStartDistance),Mathf.Sin(rndAngle)*(meteorStartDistance),0);
 
-			if(Random.value >= 0.9){
+			if(Random.value >= 0.85){
 				GameObject powerUp = Instantiate(powerUps[0],rndMeteorStart,Quaternion.identity) as GameObject;
 				powerUp.transform.parent = this.gameObject.transform;
 			} else {
